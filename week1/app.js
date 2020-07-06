4f74d536645cd31d3c1850d060b81424e4e1a87c
doroneschwartz <65253038+doroneschwartz@users.noreply.github.com>
2020-07-06 01:30:51 -0700

    Add files via upload

---
+++ week1/app.js
@@ -0,0 +1,38 @@
+(function(){
+
+  'use strict';
+
+   angular.module('LunchCheck',[])
+      .controller('LunchCheckController',LunchCheckController)
+
+      LunchCheckController.$inject= ['$scope'];
+
+      function LunchCheckController($scope) {
+            $scope.dishes='';
+            $scope.message='';
+            $scope.checked= false;
+
+            $scope.checkLunch=function(){
+
+                if ($scope.dishes.trim().length === 0) {
+                  $scope.empty = true;
+                  }
+              else{
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arraydisheswithoutempty = arrayDishes.filter(function(v) {
+                  return v.trim() !== '';
+                });
+
+                if(arraydisheswithoutempty.length <= 3){
+                  $scope.message = 'Enjoy!';
+                  }
+                else {
+                  $scope.message = 'Too Much!'
+                }
+
+              }
+            };
+          }
+})();
